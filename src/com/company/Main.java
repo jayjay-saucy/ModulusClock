399212bdfccd06451eaf7b32be393f5b3f8b1ee6
Jayllian Marquardt <jayllian.marquardt.2021@student.lhschools.org>
2020-03-12 13:47:21 -0400

    modulus operator for hours

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -16,8 +16,48 @@ public class Main {
 	System.out.println("Give me a number between 1-60");
 	minutes = key.nextLine();
 	System.out.println("The time is "+hours + minutes);
-	int number = 45;
-	String answer = number > 60 ? "Greater than 60" : "Less than or equal to";
-	System.out.println(" The answer is: "+ answer);
+
+int number = 12;
+	System.out.println(number%12);
+
+	     number =1;
+        System.out.println(number%12);
+
+        number = 2;
+        System.out.println(number%12);
+
+        number = 3;
+        System.out.println(number%12);
+
+        number = 4;
+        System.out.println(number%12);
+
+        number = 5;
+        System.out.println(number%12);
+
+         number =6;
+        System.out.println(number%12);
+
+         number =7;
+        System.out.println(number%12);
+
+         number =8;
+        System.out.println(number%12);
+
+         number =9;
+        System.out.println(number%12);
+
+         number =10;
+        System.out.println(number%12);
+
+         number =11;
+        System.out.println(number%12);
+
+         number =12;
+        System.out.println(number%13);
+
+
+
+
     }
 }
